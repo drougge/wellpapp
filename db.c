35884d1cb94ef6097637731ce44ef3dac3e14381
Carl Drougge <bearded@longhaired.org>
2016-04-10 14:17:13 +0200

    Add webm filetype.
    Yes, this should come from the config, I know.

--- db.c
+++ db.c
@@ -1433,7 +1433,7 @@ void db_read_cfg(const char *filename)
 	assert(feof(fh));
 	fclose(fh);
 	const char *ft_n = "jpeg gif png bmp dng pef nef swf avi mpeg "
-                           "mkv ogm mp4 wmv flv mov cr2 orf rw2 x3f";
+                           "mkv ogm mp4 wmv flv mov cr2 orf rw2 x3f webm";
 	cfg_parse_list(&filetype_names, ft_n);
 	assert(tagtype_names && rating_names && basedir && server_guid);
 	MD5_Final(config_md5.m, &ctx);
